c5187d711f096190fc7907744bbb05fb457f81bb
benbowden <benjamin.bowden@my.jcu.edu.au>
2023-09-21 12:04:42 +1000

    refactor function names to better represent function

--- prac_02/temperature.py
+++ prac_02/temperature.py
@@ -15,12 +15,12 @@ def main():
         if choice == "C":
             # celsius_to_fahrenheit function
             celsius = float(input("Celsius: "))
-            fahrenheit = celsius_to_fahrenheit(celsius)
+            fahrenheit = convert_celsius_to_fahrenheit(celsius)
             print(f"Result: {fahrenheit:.2f} F")
         elif choice == "F":
             # fahrenheit_to_celsius function
             fahrenheit = float(input("Fahrenheit: "))
-            celsius = fahrenheit_to_celsius(fahrenheit)
+            celsius = convert_fahrenheit_to_celsius(fahrenheit)
             print(f"Result: {celsius:.2f} F")
         else:
             print("Invalid option")
@@ -29,13 +29,13 @@ def main():
     print("Thank you.")
 
 
-def celsius_to_fahrenheit(celsius):
+def convert_celsius_to_fahrenheit(celsius):
     """ convert celsius to fahrenheit"""
     fahrenheit = celsius * 9.0 / 5 + 32
     return fahrenheit
 
 
-def fahrenheit_to_celsius(fahrenheit):
+def convert_fahrenheit_to_celsius(fahrenheit):
     """ convert fahrenheit to celsius"""
     celsius = 5 / 9 * (fahrenheit - 32)
     return celsius
