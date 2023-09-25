b4cd661c28e3ae7943af1c747cf9ad384bc7baf2
benbowden <benjamin.bowden@my.jcu.edu.au>
2023-09-26 09:17:06 +1000

    created variable result to store addition of numbers 1,2

--- prac_03/files.py
+++ prac_03/files.py
@@ -19,6 +19,7 @@ in_file = open("numbers.txt", "r")
 number1 = int(in_file.readline())
 number2 = int(in_file.readline())
 in_file.close()
-print(number1 + number2)
+result = number1 + number2
+print(result)
 
 
