89714ccdc069ca89d6dbdff4beaf71d40cb4bf7d
benbowden <benjamin.bowden@my.jcu.edu.au>
2023-09-26 09:06:33 +1000

    read, write from files

---
+++ prac_03/files.py
@@ -0,0 +1,24 @@
+
+
+
+out_file = open("name.txt", "w")
+name = input("What is your name?: ")
+print(name,file=out_file)
+out_file.close()
+
+
+
+filename = "name.txt"
+in_file = open(filename, "r")
+text = in_file.read()
+in_file.close()
+print(f"Your name is {text}")
+
+
+in_file = open("numbers.txt", "r")
+number1 = int(in_file.readline())
+number2 = int(in_file.readline())
+in_file.close()
+print(number1 + number2)
+
+
