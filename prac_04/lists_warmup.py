34b14c8bffd579ffea7be75216280e65cee56d1e
benbowden <benjamin.bowden@my.jcu.edu.au>
2023-10-01 15:35:25 +1000

    f_string'ed income input refactored months to number_of_months defined printing of income report as its own function (refactored)

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,22 @@
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# numbers[0] = first object in list = 3
+# numbers[-1] = last object in list = 2
+# numbers[3] = fourth object in list = 1
+# numbers[:-1] = list starts at beginning and finishes -1 place from end = [3, 1, 4, 1, 5, 9]
+# numbers[3:4] = list starts at fourth position and finishes at fifth position not including 5th = 1
+# 5 in numbers = 5 exists in list = true
+# 7 in numbers = 7 doesn't exist in numbers = false
+# "3" in numbers = 3(integer) exists in numbers, 3(string) doesn't exist = false
+# numbers + [6, 5, 3] = append list with 6, 5, 3, however doesn't update list = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+numbers[0] = "ten"
+
+numbers[6] = 1
+
+print(numbers[2:])
+
+print(9 in numbers)
+
+
