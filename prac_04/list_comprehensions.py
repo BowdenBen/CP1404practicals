49ff8a0d8976395d6398719f4b7ea45dbceb0a90
benbowden <benjamin.bowden@my.jcu.edu.au>
2023-10-02 16:02:43 +1000

    added list comprehension to create a list of integers from the above list of strings, numbers greater than 9

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -40,6 +40,8 @@ print(numbers)
 
 # TODO: list comprehension to create a list of only the numbers that are
 # greater than 9 from the numbers (not strings) you just created
+numbers = [int(number) for number in almost_numbers if int(number) > 9]
+print(numbers)
 
 # TODO: (more advanced) use a list comprehension and the join string method
 # to create a string (not list) of the last names for those full names longer than 11 characters
