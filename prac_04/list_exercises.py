3db19984569206c8411dae6761178e31e4a14c86
benbowden <benjamin.bowden@my.jcu.edu.au>
2023-10-02 12:42:57 +1000

    attempting error correction

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -3,8 +3,14 @@ from statistics import mean
 data = []
 
 for i in range(5):
+    try:
         number = float(input(f"Please enter a number {i + 1} "))
         data.append(number)
+    except ValueError:
+        print("Invalid number")
+        number = float(input(f"Please enter a number {i + 1} "))
+        data.append(number)
+
 print(f"The first number is {data[0]}")
 print(f"The last number is {data[4]}")
 print(f"The smallest number is {min(data)}")
