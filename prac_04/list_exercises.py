ee2d19334fe53c685d7dbb2ed0f6f3dab424f5eb
benbowden <benjamin.bowden@my.jcu.edu.au>
2023-10-02 14:37:32 +1000

    added security checker activity

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -15,3 +15,14 @@ print(f"The last number is {data[4]}")
 print(f"The smallest number is {min(data)}")
 print(f"The largest number is {max(data)}")
 print(f"The average of numbers is {mean(data)}")
+
+
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+user_id = input("Please enter your user name")
+if user_id in usernames:
+        print("Access Granted")
+else:
+        print("Access Denied")
