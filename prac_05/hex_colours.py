abe263bdb856f4a3878a6881e5dfe5d1a20944fc
benbowden <benjamin.bowden@my.jcu.edu.au>
2023-10-07 17:20:11 +1000

    copied in state_names as a start for hex_colours.py, updated variables to reflect new CONSTANT dictionary

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,15 @@
+
+COLOUR_TO_CODE = {"absolute zero": "0048ba", "acid Green": "b0bf1a", "aliceBlue": "f0f8ff",
+                  "alizarin crimson": "e32636", "amethyst": "9966cc",
+                  "bright lavender": "bf94e4", "bright lilac": "d891ef", "bright maroon": "c32148",
+                  "bright navy blue": "1974d2", "bright turquoise": "08e8de"}
+
+colour = input("Please enter colour you want a code for: ")
+colour = colour.lower()  # state_code can now accept upper and lower case input
+while colour != "":  # while state code doesn't equal nothing, allows to quit out of loop by hitting return
+    try:
+        print(colour, "is", COLOUR_TO_CODE[colour])  # as long as the key on the list is entered, print  "key" is "key value"
+    except KeyError:  # if key isn't on the list
+        print("Invalid colour")
+    colour = input("Please enter colour you want a code for: ")
+    colour = colour.lower()
