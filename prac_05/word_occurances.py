8ca1d6d3cb78bf735798c1f67cc1f53d8de6edef
benbowden <benjamin.bowden@my.jcu.edu.au>
2023-10-07 18:29:47 +1000

    refactored input text to "phrase" sorted text list alphabetically by key

--- prac_05/word_occurances.py
+++ prac_05/word_occurances.py
@@ -7,10 +7,10 @@ Write a program to count the occurrences of words in a string. The program shoul
 the counts of how many of each word are in the string.
 """
 
-text = "this is a collection of words of nice words this is a fun thing it is"
-text = list(text.split(" "))    # turn text into a list
+phrase = "this is a collection of words of nice words this is a fun thing it is"
+text = sorted(list(phrase.split(" ")))    # turn phrase into a list, sort list by key alphabetically
 word_to_count = {}      # set up dictionary
 for word in text:       # for each word in the list text
     word_to_count[word] = word_to_count.get(word, 0) + 1    # return a value for key (list), if no key set to 0, add 1
 for word, count in word_to_count.items():   # set up variables for dictionary {"word": int(count)) in "for" loop
-    print(f"{word:12}: {int(count)}")       # print out each loop, formatted to neat columns
+    print(f"{word:12}: {int(count)}")      # print out each loop, formatted to neat columns
