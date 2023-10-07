b551b8e3e78a6c9ada086b58af005c051d4178b9
benbowden <benjamin.bowden@my.jcu.edu.au>
2023-10-07 17:31:29 +1000

    added main docstring

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,4 +1,10 @@
+"""
+Use a constant dictionary of about 10 colour names and write a program that allows a user to enter a name and get the
+code, e.g., entering AliceBlue (or aliceblue - make it case-independent) should show #f0f8ff.
 
+Entering an invalid colour name should not crash the program.
+Allow the user to enter names until they enter a blank one to stop the loop.
+"""
 COLOUR_TO_CODE = {"absolute zero": "0048ba", "acid Green": "b0bf1a", "aliceBlue": "f0f8ff",
                   "alizarin crimson": "e32636", "amethyst": "9966cc",
                   "bright lavender": "bf94e4", "bright lilac": "d891ef", "bright maroon": "c32148",
