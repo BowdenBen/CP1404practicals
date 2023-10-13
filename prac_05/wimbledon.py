46e0f596d7a2ecbc74b995d076d3b7a5801be0cd
benbowden <benjamin.bowden@my.jcu.edu.au>
2023-10-13 19:27:15 +1000

    added docstrings and comments

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -19,42 +19,41 @@ def main():
     champions_data = read_file(FILENAME)
     champion_wins = count_champion_wins(champions_data)
     countries = get_countries(champions_data)
-    display_results(champion_wins, countries)
-
-
-def read_file(FILENAME):
-    """ get file and read line by line, skipping first line"""
-    champions_data = []
-    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
-        next(in_file)       # skip first line
-        for line in in_file:
-            champions_data.append(line.strip().split(","))
-    return champions_data
-
-def count_champion_wins(champions_data):
-    """ create dictionary of how many wins champions have """
-    champion_wins = {}
-    for champion in champions_data:     #
-        champion_name = champion[2]     # 3rd value in champions_data
-        if champion_name in champion_wins:
-            champion_wins[champion_name] += 1
-        else:
-            champion_wins[champion_name] = 1
-    return champion_wins
-
-
-def get_countries(champions_data):
-    countries = set()
-    for champion in champions_data:
-        countries.add(champion[1])
-    return sorted(countries)
-
-def display_results(champion_wins, countries):
     print("Champions and their number of wins:")
     for team, wins in champion_wins.items():
         print(f"{team}: {wins} times")
     print(f"\nThese {len(countries)} countries have won Wimbledon:")
     print(", ".join(countries))
+
+
+def read_file(FILENAME):
+    """ get file and read line by line, skipping first line"""
+    champions_data = []     # create champions_data list
+    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
+        next(in_file)       # skip first line
+        for line in in_file:        # iterate through every line after line 1 in file
+            champions_data.append(line.strip().split(","))      # add each line to list while formatting
+    return champions_data
+
+def count_champion_wins(champions_data):
+    """ create dictionary of how many wins champions have """
+    champion_wins = {}      # create dictionary called champions_wins
+    for champion in champions_data:     # iterate through the champions_data list
+        champion_name = champion[2]     # 3rd object in champions_data, stored in champion_name
+        if champion_name in champion_wins:      # iterate through champion_wins
+            champion_wins[champion_name] += 1       # if champion_name key exists in dictionary, add 1 to value
+        else:
+            champion_wins[champion_name] = 1       # if champion_name key doesn't exist in dictionary, add to dictionary
+    return champion_wins
+
+
+def get_countries(champions_data):
+    """ get and list the countries in alphabetical order """
+    countries = set()       # create an empty set called countries to add data to
+    for champion in champions_data:     # iterate through the champions_data list
+        countries.add(champion[1])      # for 2nd place in list of champions_data list, add object to countries
+    return sorted(countries)           # sort countries in alphabetical order
+
 
 
 main()
