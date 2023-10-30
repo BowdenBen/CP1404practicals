d8867adf33edef1c1b1244fa73c3a83890f6c200
benbowden <benjamin.bowden@my.jcu.edu.au>
2023-10-31 09:41:50 +1000

    updated class to return boolean, updated test to check boolean updated commenting

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,19 +1,19 @@
 
 
 class Guitar:
-
-    def __init__(self, name="", year=0, cost=0.0):
+    """Create Guitar class"""
+    def __init__(self, name="", year=0, cost=0.0):      # initialise class variables
         self.name = name
         self.year = year
         self.cost = cost
 
-    def __str__(self):
+    def __str__(self):      # return a string involving all variables
         return f"The {self.name}, first made in {self.year}, and costs ${self.cost:.2f}"
 
-    def get_age(self):
+    def get_age(self):      # return an age
         return 2023 - self.year
 
-    def is_vintage(self):
+    def is_vintage(self):       # return boolean if age is greater than 50
         return self.get_age() >= 50
 
 
