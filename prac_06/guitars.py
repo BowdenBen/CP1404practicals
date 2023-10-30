b4ed88982a3f240858ed008ed43ec4c78e9289fb
benbowden <benjamin.bowden@my.jcu.edu.au>
2023-10-31 09:53:26 +1000

    created guitars.py created program functions

---
+++ prac_06/guitars.py
@@ -0,0 +1,13 @@
+
+from guitar import Guitar
+
+def main():
+    """Control all functions, use class function to process list"""
+
+def get_guitar():
+    """Get user input of list of guitars(lists), include year, price"""
+    guitar_list = []
+
+
+if __name__ == "__main__":
+    main()
