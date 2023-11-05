af9470a034c490712e17c2b0317cf21dc2c91af3
benbowden <benjamin.bowden@my.jcu.edu.au>
2023-11-05 12:06:32 +1000

    updated programming_language.py to handle pointer arithmetic variable. added pointer arithmetic to languages.csv

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -7,27 +7,32 @@ Programming Language class with tests.
 class ProgrammingLanguage:
     """Represent information about a programming language."""
 
-    def __init__(self, name, typing, reflection, year):
+    def __init__(self, name, typing, reflection, pointer_arithmetic, year):
         """Construct a ProgrammingLanguage from the given values."""
         self.name = name
         self.typing = typing
         self.reflection = reflection
+        self.pointer_arithmetic = pointer_arithmetic
         self.year = year
 
     def __repr__(self):
         """Return string representation of a ProgrammingLanguage."""
-        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, Pointer Arithmetic={self.pointer_arithmetic}, First appeared in {self.year}"
 
     def is_dynamic(self):
         """Determine if language is dynamically typed."""
         return self.typing == "Dynamic"
 
+    def has_pointer_arithmetic(self):
+        """Check if the language supports pointer arithmetic."""
+        return self.pointer_arithmetic
+
 
 def run_tests():
     """Run simple tests/demos on ProgrammingLanguage class."""
-    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, False, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, False,1991)
 
     languages = [ruby, python, visual_basic]
     print(python)
@@ -35,6 +40,11 @@ def run_tests():
     print("The dynamically typed languages are:")
     for language in languages:
         if language.is_dynamic():
+            print(language.name)
+
+    print("Languages that support pointer arithmetic:")
+    for language in languages:
+        if language.has_pointer_arithmetic():
             print(language.name)
 
 
