544dbf491b51a9d182f837ef71e84b38946defc8
benbowden <benjamin.bowden@my.jcu.edu.au>
2023-11-18 16:26:35 +1000

    imported App, Builder and StringProperty from Kivy. created class and defined where build will pull information from

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,8 +1,22 @@
 """
-Miles to Kilometer app
+Miles to Kilometres app
 Benjamin Bowden
 
 est. time: 1 hour
 actual time:
 """
 
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+
+MILES_KM_CONVERSION = 1.60934
+
+class ConvertMilesKm(App):
+    """ create app"""
+
+    def build(self):
+        """ build the app from the kv file """
+        self.title = "Convert Miles to Kilometres"  # App window title
+        self.root = Builder.load_file("convert_miles_km.kv")    # Build app using .kv file
+        return self.root
