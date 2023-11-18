4288269f3af4fb4c22e7b05e82ae284a2e88407b
benbowden <benjamin.bowden@my.jcu.edu.au>
2023-11-18 18:02:14 +1000

    completed StrinProperty for outpot, added an up and down function that increases and decreases by (1) one.

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -3,33 +3,45 @@ Miles to Kilometres app
 Benjamin Bowden
 
 est. time: 1 hour
-actual time:
+actual time: 3 hour
 """
 
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import StringProperty
 
-MILES_KM_CONVERSION = 1.60934
+MILES_KM_CONVERSION_FACTOR = 1.60934
 
-class ConvertMilesKmApp(App):
-    """ create app"""
+
+class ConvertMilesKm(App):
+    """Kivy app that converts miles to Kilometers."""
+    output_distance = StringProperty()
 
     def build(self):
-        """ build the app from the kv file """
-        self.title = "Convert Miles to Kilometres"  # App window title
-        self.root = Builder.load_file("convert_miles_km.kv")    # Build app using .kv file
+        """Build the Kivy App."""
+        self.title = "Convert Miles to Kilometers"
+        self.root = Builder.load_file('convert_miles_km.kv')
+        self.output_distance = "Enter a distance and press enter"
         return self.root
 
-    def handle_calculation(self):
-        """ handle the calculation"""
+    def handle_increment(self, increment):
+        """Increment and decrement input with error checking."""
         try:
-            miles_input = float(self.root.ids.input_miles.text)
-            converted_input = miles_input * MILES_KM_CONVERSION
-            self.root.ids.output_kilometre.text = str(converted_input)
-        except ValueError:  # won't action if number isn't entered
-            pass
+            current_input = float(self.root.ids.input_distance.text)
+        except ValueError:
+            self.root.ids.input_distance.text = '0.0'
+            current_input = float(self.root.ids.input_distance.text)
+        current_input += increment
+        self.root.ids.input_distance.text = str(current_input)
+
+    def convert(self):
+        """Convert from miles to kilometers using MILES_KM_CONVERSION_FACTOR."""
+        try:
+            current_input = float(self.root.ids.input_distance.text)
+            current_input *= MILES_KM_CONVERSION_FACTOR
+            self.output_distance = str(current_input)
+        except ValueError:
+            self.output_distance = '0.0'
 
 
-
-ConvertMilesKmApp().run()
+ConvertMilesKm().run()
