ce4ed2a5e92ef30a3d814234862410d33c219a33
benbowden <benjamin.bowden@my.jcu.edu.au>
2023-11-19 18:25:54 +1000

    new initialise of siver service, adding fanciness to car and taxi class objects

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -6,3 +6,9 @@ from taxi import Taxi
 
 
 class SilverServiceTaxi(Taxi):
+    """ A child of the taxi class, therefore inherits from taxi and car classes """
+
+    def __init__(self, name, fuel, fanciness: float):
+        """ passing in Car class objects, adding fanciness object """
+        super().__init__(name, fuel)
+        self.price_per_km = Taxi.price_per_km * fanciness  # fanciness is a scaler of all taxi's price per km
