ea059f954dbdbad8c6e33acc5628651d5905a9a3
benbowden <benjamin.bowden@my.jcu.edu.au>
2023-11-19 17:51:42 +1000

    initialised class with reliability added to parent objects

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -7,3 +7,8 @@ from car import Car
 
 class UnreliableCar(Car):
     """ Car class with a reliability object """
+
+    def __init__(self, name, fuel, reliability: float):
+        """ Initialise Car object, pulling from Car class parent, and adding reliability"""
+        super().__init__(name, fuel)
+        self.reliability = reliability
