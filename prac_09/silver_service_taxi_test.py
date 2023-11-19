f73184088328961e8ca0f828850411ca86aa750c
benbowden <benjamin.bowden@my.jcu.edu.au>
2023-11-19 18:46:27 +1000

    updated test to check for flagfall, and calculate total fare

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -6,5 +6,7 @@ testing Silver Service Taxi Class.
 from silver_service_taxi import SilverServiceTaxi
 
 silver_service_taxi = SilverServiceTaxi("Black & White", 100, fanciness=1.5)
-print(silver_service_taxi)
 silver_service_taxi.start_fare()
+silver_service_taxi.drive(30)
+print(silver_service_taxi)
+print(f"Premium Fare: ${silver_service_taxi.get_fare():.2f} includes flagfall of: ${silver_service_taxi.flagfall:.2f}")
