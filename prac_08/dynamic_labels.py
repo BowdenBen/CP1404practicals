96418963b6f046ca6a6b0645f91e2e4cd2e9295a
benbowden <benjamin.bowden@my.jcu.edu.au>
2023-11-19 10:11:51 +1000

    imported required tools into dynamic_labels.py.

---
+++ prac_08/dynamic_labels.py
@@ -0,0 +1,10 @@
+"""
+Dynamic Labels exercise.
+Benjamin Bowden
+
+Estimated time: 30 min
+actual time:
+"""
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
