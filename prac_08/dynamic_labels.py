a1fba6f24e4b788e80d77f61d6bbe51e7813e7fe
benbowden <benjamin.bowden@my.jcu.edu.au>
2023-11-19 10:32:47 +1000

    copied in build function, updated to fit new app

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -13,3 +13,13 @@ NAMES = ["Gregory Peck", "Dr NoGrundles", "Lenny"]
 
 class DynamicLabelsApp(App):
     """ Create dynamic labels in a GUI from a list of NAMES """
+
+    def build(self):
+        """Build the Kivy GUI."""
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        label = Label(text=NAMES, font_size='52')
+        return self.root
+
+
+DynamicLabelsApp().run()
