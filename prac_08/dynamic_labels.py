5cdde44860b2d1fded6c7c89eaa97200dd4acb8c
benbowden <benjamin.bowden@my.jcu.edu.au>
2023-11-19 10:25:21 +1000

    created list of NAMES. created class to handle a dynamic gui interface using said NAMES.

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -8,3 +8,8 @@ actual time:
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.label import Label
+
+NAMES = ["Gregory Peck", "Dr NoGrundles", "Lenny"]
+
+class DynamicLabelsApp(App):
+    """ Create dynamic labels in a GUI from a list of NAMES """
