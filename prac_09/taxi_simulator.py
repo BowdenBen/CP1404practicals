2acc770adb68ba8a25246e340613593e20355b06
benbowden <benjamin.bowden@my.jcu.edu.au>
2023-11-19 19:33:31 +1000

    set current_taxi to None

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -11,11 +11,15 @@ MENU = """q)uit, c)hoose taxi, d)rive"""
 
 def main():
     """Get an input choice from MENU"""
+    current_taxi = None
 
     print("Let's drive!")
     print(MENU)
     choice = input(">>> ").lower()
     while choice != 'q':
+        if choice == 'd':
+            if current_taxi is None:
+                print("You need to choose a taxi before you can drive")
 
 
 def drive_taxi():
