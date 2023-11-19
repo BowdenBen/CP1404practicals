45d29568a6d9e36b9b4a3462ad3d34bb21df9e5c
benbowden <benjamin.bowden@my.jcu.edu.au>
2023-11-19 18:40:23 +1000

    added get_fare override which pulls get_fare from Taxi class then adds a flagfall to it to create a new get_fare

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -7,8 +7,13 @@ from taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
     """ A child of the taxi class, therefore inherits from taxi and car classes """
+    flagfall = 4.50
 
     def __init__(self, name, fuel, fanciness: float):
         """ passing in Car class objects, adding fanciness object """
         super().__init__(name, fuel)
         self.price_per_km = Taxi.price_per_km * fanciness  # fanciness is a scaler of all taxi's price per km
+
+    def get_fare(self):
+        """Override get_fare from Taxi Class to add a premium flagfall"""
+        return super().get_fare() + self.flagfall
